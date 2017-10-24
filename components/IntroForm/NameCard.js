0b740495811b400f2a932690483c65cfc01c4c31
nwshane <django09@gmail.com>
2017-10-23 22:49:39 -0400

    Added floating label text and validation to NameCard.

--- components/IntroForm/NameCard.js
+++ components/IntroForm/NameCard.js
@@ -1,24 +1,30 @@
 import TextField from 'material-ui/TextField'
 import NextCardButton from './NextCardButton'
 
+const isValidName = (name) => (
+  name.length >= 0 && name.trim().split(' ').length >= 2
+)
+
 export default ({name, changeName, goToNextCard}) => (
   <section>
     <div />
     <div>
       <div>
         <label htmlFor='name_input'>
-          What's your name?
+          Hey there!<br/>What's your name?
         </label>
       </div>
       <TextField
         id='name_input'
         name='name'
         value={name}
+        floatingLabelText='Please enter your full name.'
         onChange={changeName}
       />
     </div>
     <div>
       <NextCardButton
+        disabled={!isValidName(name)}
         onClick={goToNextCard}
       />
     </div>
